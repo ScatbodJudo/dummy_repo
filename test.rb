79c4c51737531a8eb0779c578b14f0fa4b01f067
Todd Jacobus <rtjacobus1@gmail.com>
2016-09-20 13:46:20 -0600

    trololololol

--- test.rb
+++ test.rb
@@ -2,4 +2,8 @@ puts "This is a test...I'm totally not a robot."
 
 puts "I made a change after I staged a file for commit....what???"
 
-puts "show me the git diff"
+puts "show me the git diff???"
+
+do something |t|
+	puts "I am doing something..." + t.to_s
+end
